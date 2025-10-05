1df5b9245124f0991bf6613bb594ce6c248ff4c1
Mathieu Moalic <matmoa@pm.me>
2025-10-05 12:19:17 +0200

    Cleanup flake.nix again

--- flake.nix
+++ flake.nix
@@ -1,91 +1,73 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        android_sdk.accept_license = true;
+      };
+    };
+
+    androidSdk =
+      (pkgs.androidenv.composeAndroidPackages {
+        platformVersions = ["35"];
+        buildToolsVersions = ["35.0.0"];
+
+        ndkVersions = ["27.0.12077973"];
+        includeNDK = true;
+
+        cmakeVersions = ["3.22.1"];
+        includeCmake = true;
+
+        includeEmulator = false;
+      }).androidsdk;
+
+    sdkRoot = "${androidSdk}/libexec/android-sdk";
+  in {
+    packages.${system}.default = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
+      pname = "blaz";
+      version = "0.1.0";
+      src = ./backend;
+      cargoLock.lockFile = ./backend/Cargo.lock;
+      CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+      RUSTFLAGS = "-C target-feature=+crt-static";
+      cargoBuildFlags = ["--locked"];
+      doCheck = false;
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      name = "dev-shell";
+      packages = with pkgs; [
+        flutter
+        dart
+        androidSdk
+        android-tools
+        jdk17
+        just
+
+        rustc
+        cargo
+        clippy
+        rustfmt
+        pkg-config
+        mold
+      ];
+
+      shellHook = ''
+        export RUSTFLAGS="-C link-arg=-fuse-ld=mold"
+
+        cat > "flutter/android/local.properties" <<EOF
+              sdk.dir=${sdkRoot}
+              cmake.dir=${sdkRoot}/cmake/3.22.1
+              ndk.dir=${sdkRoot}/ndk/27.0.12077973
+              flutter.buildMode=debug
+              flutter.versionName=1.0.0
+              flutter.versionCode=1
+        EOF
+      '';
+    };
   };
-
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-          android_sdk.accept_license = true;
-        };
-      };
-
-      muslTarget =
-        {
-          x86_64-linux = "x86_64-unknown-linux-musl";
-          aarch64-linux = "aarch64-unknown-linux-musl";
-        }.${
-          system
-        };
-
-      rustSrc = ./backend;
-      rustLockfile = ./backend/Cargo.lock;
-
-      androidSdk =
-        (pkgs.androidenv.composeAndroidPackages {
-          platformVersions = ["35" "34"];
-          buildToolsVersions = ["35.0.0" "34.0.0"];
-          ndkVersions = ["27.0.12077973"];
-          includeNDK = true;
-          includeEmulator = false;
-          cmakeVersions = ["3.22.1"];
-          includeCmake = true;
-        }).androidsdk;
-
-      sdkRoot = "${androidSdk}/libexec/android-sdk";
-    in {
-      packages.default = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
-        pname = "blaz";
-        version = "0.1.0";
-        src = rustSrc;
-        cargoLock.lockFile = rustLockfile;
-
-        CARGO_BUILD_TARGET = muslTarget;
-        RUSTFLAGS = "-C target-feature=+crt-static";
-
-        cargoBuildFlags = ["--locked"];
-        doCheck = false;
-      };
-
-      devShells.default = pkgs.mkShell {
-        name = "dev-shell";
-        packages = with pkgs; [
-          flutter
-          dart
-          androidSdk
-          android-tools
-          jdk17
-          just
-
-          # Rust
-          rustc
-          cargo
-          clippy
-          rustfmt
-          pkg-config
-          mold
-        ];
-
-        shellHook = ''
-          export RUSTFLAGS="-C link-arg=-fuse-ld=mold"
-
-          cat > "flutter/android/local.properties" <<EOF
-                sdk.dir=${sdkRoot}
-                cmake.dir=${sdkRoot}/cmake/3.22.1
-                ndk.dir=${sdkRoot}/ndk/27.0.12077973
-                flutter.buildMode=debug
-                flutter.versionName=1.0.0
-                flutter.versionCode=1
-          EOF
-        '';
-      };
-    });
 }
